d457a206de101b6dd6e958d0438be5bd7cd241de
MerrickCampus <merrick.dida@le-campus-numerique.fr>
2023-11-21 14:26:22 +0100

    Adding calendars and trip

--- SQLite_exec_ed.sql
+++ SQLite_exec_ed.sql
@@ -22,3 +22,34 @@ CREATE TABLE route (
 	CONSTRAINT route_pk PRIMARY KEY (id),
 	CONSTRAINT route_agency_fk FOREIGN KEY (agency_id) REFERENCES agency(id)
 );
+
+CREATE TABLE calendar (
+	service_id varchar(255) NOT NULL,
+	monday bool NULL,
+	tuesday bool NULL,
+	wednesday bool NULL,
+	thursday bool NULL,
+	friday bool NULL,
+	saturday bool NULL,
+	sunday bool NULL,
+	start_date date NULL,
+	end_date date NULL,
+    CONSTRAINT calendar_pkey PRIMARY KEY (uid),
+	CONSTRAINT calendar_service_id_feed_id_key UNIQUE (service_id, feed_id)
+);
+
+
+CREATE TABLE calendar_date (
+	service_id varchar(255) NOT NULL,
+	"date" date NOT NULL,
+    exception_type int4 NOT NULL,
+    CONSTRAINT calendar_date_pkey PRIMARY KEY (uid),
+	CONSTRAINT calendar_date_service_id_date_feed_id_key UNIQUE (service_id, date, feed_id)
+);
+
+CREATE TABLE trip (
+	trip_id varchar(255) NOT NULL,
+	route_id varchar(255) NOT NULL,
+	service_id varchar(255) NOT NULL,
+    CONSTRAINT trip_pkey PRIMARY KEY (uid),
+);	
