1d4a1a706f083685045b26fc8a06fe522efa6daa
MerrickCampus <merrick.dida@le-campus-numerique.fr>
2023-11-22 10:09:13 +0100

    Changing data shape to match tag dataset

--- SQLite_exec_ed.sql
+++ SQLite_exec_ed.sql
@@ -9,27 +9,25 @@ DROP TABLE IF EXISTS fare_rules;
 DROP TABLE IF EXISTS fare_attributes;
 
 CREATE TABLE agency (
-	id varchar(255) NOT NULL,     
-	"name" varchar(255) NOT NULL, 
-	url varchar(512) NULL,        
-	timezone varchar(255) NULL,   
-	lang varchar(20) NULL,        
-	phone varchar(255) NULL,      
-	fare_url varchar(512) NULL,   
-	email varchar(255) NULL,      
-	PRIMARY KEY (id)              
+	agency_id varchar(255) NOT NULL,     
+	agency_name varchar(255) NOT NULL, 
+	agency_url varchar(512) NULL,        
+	agency_timezone varchar(255) NULL,   
+	agency_lang varchar(20) NULL,        
+	agency_phone varchar(255) NULL,      
+	PRIMARY KEY (agency_id)              
 );
 
 CREATE TABLE route (
-	route_id varchar(255) NOT NULL,       
 	agency_id varchar(255) NULL,          
-	short_name varchar(255) NOT NULL,     
-	long_name varchar(255) NOT NULL,      
-	description varchar(255) NULL,        
-	"type" int4 NULL,                     
-	url varchar(255) NULL,                
+	route_id varchar(255) NOT NULL,       
+	route_short_name varchar(255) NOT NULL,     
+	route_long_name varchar(255) NOT NULL,      
+	route_type int4 NOT NULL,
+	route_color varchar(255) NULL,
+	route_text_color varchar(255) NULL,            
 	PRIMARY KEY (route_id), 
-	FOREIGN KEY (agency_id) REFERENCES agency(id) 
+	FOREIGN KEY (agency_id) REFERENCES agency(agency_id) 
 );
 
 CREATE TABLE calendar (
@@ -50,7 +48,7 @@ CREATE TABLE calendar (
 
 CREATE TABLE calendar_date (
 	service_id varchar(255) NOT NULL,
-	"date" date NOT NULL,
+	date date NOT NULL,
     exception_type int4 NOT NULL,
     CONSTRAINT calendar_date_pkey PRIMARY KEY (service_id),
 	CONSTRAINT service_id FOREIGN KEY (service_id) REFERENCES calendar(service_id),
@@ -58,10 +56,15 @@ CREATE TABLE calendar_date (
 );
 
 CREATE TABLE trip (
-	trip_id varchar(255) NOT NULL,
 	route_id varchar(255) NOT NULL,
+	trip_id varchar(255) NOT NULL,
+	trip_headsign varchar(255) NULL,
 	service_id varchar(255) NOT NULL,
-    CONSTRAINT trip_pkey PRIMARY KEY (trip_id)
+    direction_id int4 NULL,
+	shape_id varchar(255) NULL,
+	whellchair_accessible int4 NULL,
+	bikes_allowed int4 NULL,
+	CONSTRAINT trip_pkey PRIMARY KEY (trip_id)
 	CONSTRAINT route_id FOREIGN KEY (route_id) REFERENCES route(route_id)
 	CONSTRAINT service_id FOREIGN KEY (service_id) REFERENCES calendar(service_id)
 );	
@@ -70,25 +73,24 @@ CREATE TABLE stop (
 	stop_id int4 NOT NULL UNIQUE,
 	stop_code int4 NOT NULL,
 	stop_name varchar(255) NULL,
-	stop_desc varchar(255) NULL,
 	stop_lat int4 NOT NULL,
 	stop_lon int4 NOT NULL,
 	zone_id int4 NULL,
-	stop_url varchar(255) NOT NULL,
 	location_type varchar(255) NULL,
+	parent_station int4 NULL,
 	wheelchair_boarding int4 NULL,
-	level_id int4 NULL,
-	platform_code varchar(255) NULL,	
 	PRIMARY KEY (stop_id)
 );
 
 CREATE TABLE stop_time (
-	stop_times_id int NOT NULL UNIQUE,
+	trip_id varchar(255) NOT NULL,
 	stop_id int  NOT NULL,
 	arrival_time DATETIME NOT NULL,
 	departure_time DATETIME NOT NULL,
 	stop_sequence str NOT NULL,	
-	PRIMARY KEY (stop_times_id )
+	pickup_type int4 NULL,
+	PRIMARY KEY (stop_id, trip_id)
+	FOREIGN KEY (trip_id) REFERENCES trip(trip_id)
 	FOREIGN KEY (stop_id) REFERENCES stop(stop_id)
 );	
 
@@ -108,7 +110,6 @@ CREATE TABLE fare_attributes (
 	currency_type varchar(256),
 	payment_method int,
 	transfers int,
-	agency_id varchar(256),
 	transfer_duration int,
 	PRIMARY KEY (fare_id)
 );
